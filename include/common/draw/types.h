ca3f59fdeaa55ec4bb2256a05baf35f1f03f3a30
topilski <atopilski@gmail.com>
2017-09-08 17:20:09 +0300

    Size class

---
+++ include/common/draw/types.h
@@ -0,0 +1,61 @@
+/*  Copyright (C) 2014-2017 FastoGT. All right reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+        * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following disclaimer
+    in the documentation and/or other materials provided with the
+    distribution.
+        * Neither the name of FastoGT. nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include <string>
+
+namespace common {
+namespace draw {
+
+struct Size {
+  Size();  // invalid size
+  Size(int width, int height);
+
+  bool IsValid() const;
+  bool Equals(const Size& sz) const;
+
+  int width;
+  int height;
+};
+
+inline bool operator==(const Size& left, const Size& right) {
+  return left.Equals(right);
+}
+
+inline bool operator!=(const Size& left, const Size& right) {
+  return !left.Equals(right);
+}
+
+bool IsValidSize(int width, int height);
+
+}  // namespace draw
+std::string ConvertToString(const draw::Size& value);
+bool ConvertFromString(const std::string& from, draw::Size* out);
+}  // namespace common
