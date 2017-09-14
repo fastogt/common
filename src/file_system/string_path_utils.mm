28184305b3b8959b4347996401d7a6c3333319ff
topilski <atopilski@gmail.com>
2017-09-14 17:08:55 +0300

    Fix MacOSX

--- src/file_system/string_path_utils.mm
+++ src/file_system/string_path_utils.mm
@@ -1,11 +1,12 @@
-#include "common/file_system.h"
+#include <common/file_system/string_path_utils.h>
 
-#import <Foundation/NSPathUtilities.h>
 #import <Foundation/NSBundle.h>
+#import <Foundation/NSPathUtilities.h>
 #import <Foundation/NSString.h>
 
 namespace common {
 namespace file_system {
+
 std::string bundle_pwd() {
   NSString* bundlePath = [[NSBundle mainBundle] resourcePath];
   NSString* secondParentPath = [[bundlePath stringByDeletingLastPathComponent] stringByDeletingLastPathComponent];
