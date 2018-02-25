6e61ff7c598c4b51f364d15dfd0d813b5e4b0bb4
Topilski <atopilski@gmail.com>
2018-02-25 02:32:48 -0800

    Fix MacOSX build

--- src/threads/posix_thread.cpp
+++ src/threads/posix_thread.cpp
@@ -56,7 +56,7 @@ typedef struct cpu_set { uint32_t count; } cpu_set_t;
 
 int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t* cpu_set) {
   thread_port_t mach_thread;
-  int core = 0;
+  size_t core = 0;
 
   for (core = 0; core < 8 * cpu_size; core++) {
     if (CPU_ISSET(core, cpu_set)) {
@@ -64,7 +64,7 @@ int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t* cpu_set
     }
   }
 
-  thread_affinity_policy_data_t policy = {core};
+  thread_affinity_policy_data_t policy = {static_cast<integer_t>(core)};
   mach_thread = pthread_mach_thread_np(thread);
   thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
   return 0;
