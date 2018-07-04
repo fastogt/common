2d4e5cb40ee89292a33aa200c6797c9e3ce6bad2
topilski <atopilski@gmail.com>
2018-07-04 01:47:07 -0400

    Fix unit tests

--- tests/unit_test_license.cpp
+++ tests/unit_test_license.cpp
@@ -3,11 +3,9 @@
 #include <common/license/gen_hardware_hash.h>
 
 TEST(License, Generate) {
-  std::string hdd_hash_text;
-  bool hdd_hash = common::license::GenerateHardwareHash(common::license::HDD, &hdd_hash_text);
-  ASSERT_TRUE(hdd_hash);
+  bool hdd_hash = common::license::GenerateHardwareHash(common::license::HDD, nullptr);
+  ASSERT_FALSE(hdd_hash);
 
-  std::string mach_hash_text;
-  bool mach_hash = common::license::GenerateHardwareHash(common::license::MACHINE_ID, &mach_hash_text);
-  ASSERT_TRUE(mach_hash);
+  bool mach_hash = common::license::GenerateHardwareHash(common::license::MACHINE_ID, nullptr);
+  ASSERT_FALSE(mach_hash);
 }
