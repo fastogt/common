3663d8cab0875f9a21c355f0f275fa6b61b1e0a4
topilski <atopilski@gmail.com>
2018-07-20 02:15:18 -0400

    Hot fix for RPI

--- src/license/utils.cpp
+++ src/license/utils.cpp
@@ -18,8 +18,12 @@
 
 namespace {
 void native_cpuid(unsigned int* eax, unsigned int* ebx, unsigned int* ecx, unsigned int* edx) {
+#if defined(ARCH_CPU_X86_FAMILY)
   /* ecx is often an input as well as an output. */
   asm volatile("cpuid" : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx) : "0"(*eax), "2"(*ecx) : "memory");
+#else
+
+#endif
 }
 
 }  // namespace
