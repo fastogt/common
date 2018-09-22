a6f568edfef227673ca5efe2ffe58077f9dfd36a
topilski <atopilski@gmail.com>
2018-09-22 06:06:06 +0300

    ignore_result

--- include/common/macros.h
+++ include/common/macros.h
@@ -259,6 +259,17 @@ void immediate_exit();
   COMPILE_ASSERT(sizeof(str) == size, "sizeof " STRINGIZE(str) " must be " STRINGIZE(size) " byte" \
                                                                                            "!")
 
+// Used to explicitly mark the return value of a function as unused. If you are
+// really sure you don't want to do anything with the return value of a function
+// that has been marked WARN_UNUSED_RESULT, wrap it with this. Example:
+//
+//   std::unique_ptr<MyType> my_var = ...;
+//   if (TakeOwnership(my_var.get()) == SUCCESS)
+//     ignore_result(my_var.release());
+//
+template <typename T>
+inline void ignore_result(const T&) {}
+
 template <typename T>
 inline void destroy(T** v) {
   if (!v) {
