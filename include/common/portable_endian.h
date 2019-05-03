c27fd6c14ed2629cf48f90b3692db961ba452895
topilski <atopilski@gmail.com>
2019-05-03 01:35:31 -0400

    Typo

--- include/common/portable_endian.h
+++ include/common/portable_endian.h
@@ -31,7 +31,7 @@
 
 #include <common/macros.h>
 
-#if defined(OS_MACOSX) || defied(OS_IOS)
+#if defined(OS_MACOSX) || defined(OS_IOS)
 #include <libkern/OSByteOrder.h>
 #define htobe16(x) OSSwapHostToBigInt16(x)
 #define htole16(x) OSSwapHostToLittleInt16(x)
