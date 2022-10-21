263501d3ee9f4bdd5a6f96e93db51396be009f4d
topilski <atopilski@gmail.com>
2022-10-21 08:44:55 -0400

    HHash

---
+++ include/common/daemon/commands/hhash_info.h
@@ -0,0 +1,63 @@
+/*  Copyright (C) 2014-2022 FastoGT. All right reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+        * Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following disclaimer
+    in the documentation and/or other materials provided with the
+    distribution.
+        * Neither the name of FastoGT. nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include <common/license/types.h>
+#include <common/optional.h>
+#include <common/serializer/json_serializer.h>
+
+#include <string>
+
+namespace common {
+namespace daemon {
+namespace commands {
+
+class HardwareHashInfo : public common::serializer::JsonSerializer<HardwareHashInfo> {
+ public:
+  typedef JsonSerializer<HardwareHashInfo> base_class;
+  typedef license::hardware_hash_t raw_license_t;
+  typedef Optional<raw_license_t> license_t;
+  HardwareHashInfo();
+  explicit HardwareHashInfo(license_t license);
+
+  bool IsValid() const;
+  license_t GetLicense() const;
+
+ protected:
+  Error DoDeSerialize(json_object* serialized) override;
+  Error SerializeFields(json_object* out) const override;
+
+ private:
+  license_t license_;
+};
+
+}  // namespace commands
+}  // namespace daemon
+}  // namespace common
